b7c7d09428230f6f7ed1b4623ef6404813a1c992
atendimento <atendimento@pandorainternet.net>
2025-11-11 20:19:16 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,10 @@
+
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { base44 } from "@/api/base44Client";
 import { useQuery } from "@tanstack/react-query";
-import * as CustomAuth from "@/CustomAuth";
+import * as CustomAuth from "./CustomAuth"; // Changed from "@/CustomAuth" to "./CustomAuth"
 import {
   LayoutDashboard,
   Building2,
