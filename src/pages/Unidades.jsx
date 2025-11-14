c8c70aeaaaa78db42f6d2d3727229872549e58c6
atendimento <atendimento@pandorainternet.net>
2025-11-14 11:30:08 +0000

    File changes

--- src/pages/Unidades.jsx
+++ src/pages/Unidades.jsx
@@ -118,7 +118,7 @@ export default function Unidades() {
     return (
       <div className="p-4 md:p-8">
         <UnidadeForm
-          item={editingItem}
+          unidade={editingItem}
           loteamentos={loteamentos}
           clientes={clientes}
           onSubmit={handleSubmit}
